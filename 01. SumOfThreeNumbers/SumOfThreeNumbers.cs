110b862fc03f103a942ab55d590dad227421933c
asta23 <asta_23@abv.bg>
2016-03-30 12:10:00 +0300

    repair a file

--- 01. SumOfThreeNumbers/SumOfThreeNumbers.cs
+++ 01. SumOfThreeNumbers/SumOfThreeNumbers.cs
@@ -3,6 +3,7 @@
     {
         static void Main()
         {
+            Console.WriteLine("Write three numbers: ");
             double a = double.Parse(Console.ReadLine());
             double b = double.Parse(Console.ReadLine());
             double c = double.Parse(Console.ReadLine());
